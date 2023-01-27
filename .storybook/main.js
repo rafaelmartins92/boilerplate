57ceecf87267df66cbca0c5895f450f6b6d7cfbc
Rafael Martins <rafael.martins92@outlook.com>
2023-01-26 23:06:30 -0300

    chore: use storybook essentials and controls

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5'
+  }
+}
